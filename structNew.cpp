b4fa7a6ee03d02fbe427c9532c61676d11ffc646
raffibromabar-web <raffibromabar@gmail.com>
2026-04-06 16:18:31 +0700

    tampilan nama, alamat, umur

--- structNew.cpp
+++ structNew.cpp
@@ -17,4 +17,8 @@ int main (){
     cout << "Umur : " ;
     cin >> mhs.umur;
     cout << endl;
+    // tampilkan
+    cout << " Nama : " << mhs.nama << endl;
+    cout << " Alamat : " << mhs.alamat << endl;
+    cout << " Umur : " << mhs.umur << endl;
 }
